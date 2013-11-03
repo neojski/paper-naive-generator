9c01c8770e2255aaba009f245adc9b7c4c3345ae
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-11-03 20:35:50 +0100

    Add if tags null check for onet article source

--- src/main/java/bullshit_paper/OnetArticleSource.java
+++ src/main/java/bullshit_paper/OnetArticleSource.java
@@ -6,8 +6,6 @@ import java.net.URLEncoder;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,13 +13,14 @@ import org.jsoup.nodes.Element;
 public class OnetArticleSource implements IArticleSource {
 
     private final String chromeUserAgent = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
+    private final String searchURL = "http://plejada.onet.pl/szukaj/wiadomosci,1,1,szukaj.html?qt=";
 
     private List<IComment> retrieveComments(Document doc) {
         LinkedList<IComment> res = new LinkedList<>();
         for (Element e : doc.select("#forum .k_nForum_ReaderItem")) {
             String commentAuthor = e.select(".k_author").text();
             String commentContent = e.select(".k_content").text();
-            res.add(new Comment(commentContent, commentAuthor, null)); // TODO: date
+            res.add(new Comment(commentContent, commentAuthor, new Date())); // TODO: date
         }
         return res;
     }
@@ -29,7 +28,7 @@ public class OnetArticleSource implements IArticleSource {
     private IArticle retrieveArticle(String url) {
         try {
             Document doc = Jsoup.connect(url).userAgent(chromeUserAgent).get();
-            
+
             String content = doc.select(".detail.intext").text();
             String title = doc.select("#mainTitle").text();
 
@@ -40,13 +39,10 @@ public class OnetArticleSource implements IArticleSource {
     }
 
     private String buildQuery(List<String> tags) {
-        if (tags == null) {
-            return null;
-        }
         try {
             StringBuilder sb = new StringBuilder();
             for (String tag : tags) {
-                    sb.append(URLEncoder.encode(tag, "UTF-8")).append("+");
+                sb.append(URLEncoder.encode(tag, "UTF-8")).append("+");
             }
             return sb.substring(0, sb.length() - 1);
         } catch (UnsupportedEncodingException ex) {
@@ -56,9 +52,12 @@ public class OnetArticleSource implements IArticleSource {
 
     @Override
     public List<IArticle> getArticles(List<String> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return null;
+        }
         try {
             LinkedList<IArticle> res = new LinkedList<>();
-            String url = "http://plejada.onet.pl/szukaj/wiadomosci,1,1,szukaj.html?qt=" + buildQuery(tags);
+            String url = searchURL + buildQuery(tags);
             Document doc = Jsoup.connect(url).userAgent(chromeUserAgent).get();
             for (Element e : doc.select("#searchProxyMain .link a")) {
                 String articleURL = e.attr("href");
