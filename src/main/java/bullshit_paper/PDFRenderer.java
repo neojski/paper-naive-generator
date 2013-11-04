d77749e4dc69fad7de26a823b20fe6a868445e80
michalx2 <michalx2@gmail.com>
2013-11-04 01:45:33 +0100

    add simple PDFRenderer and a test

---
+++ src/main/java/bullshit_paper/PDFRenderer.java
@@ -0,0 +1,36 @@
+package bullshit_paper;
+
+import java.util.*;
+import java.io.*;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import java.text.*;
+
+public class PDFRenderer implements IRenderer
+{
+	public void Render(OutputStream stream, String title, java.util.List<IArticle> articles)
+	{
+		try {
+			Document doc = new Document();
+			PdfWriter.getInstance(doc, stream);
+			doc.addTitle(title);
+			doc.open();
+			Font paperTitleFont = FontFactory.getFont(FontFactory.TIMES_BOLD, 20.0f);
+			Font titleFont = FontFactory.getFont(FontFactory.TIMES_BOLD, 12.0f);
+			Font dateFont = FontFactory.getFont(FontFactory.TIMES_ITALIC, 8.0f);
+			Font contentFont = FontFactory.getFont(FontFactory.TIMES, 10.0f);
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			Chapter chapter = new Chapter(new Paragraph(title, paperTitleFont), 0);
+			chapter.setNumberDepth(0);
+			for (IArticle art : articles) {
+				Section section = chapter.addSection(new Paragraph(art.getTitle(), titleFont));
+				section.setNumberDepth(0);
+				section.add(new Paragraph(dateFormat.format(art.getDate()), dateFont));
+				section.add(new Paragraph(art.getContent(), contentFont));				
+			}
+			doc.add(chapter);
+			doc.close();
+		}
+		catch (DocumentException ex) { }
+	}
+}
