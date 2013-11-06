ef4db3faf89fca2c8317469ca8d11114f856968f
Marcin <dziadus.marcin@gmail.com>
2013-11-06 16:44:48 +0100

    Add date support to PudelekArticleSource

--- src/main/java/bullshit_paper/PudelekArticleSource.java
+++ src/main/java/bullshit_paper/PudelekArticleSource.java
@@ -1,6 +1,9 @@
 package bullshit_paper;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -9,6 +12,7 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class PudelekArticleSource implements IArticleSource{
 	private final String chromeUserAgent = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
@@ -53,27 +57,50 @@ public class PudelekArticleSource implements IArticleSource{
 		return articles;
 	}
 	
+	private Date parseDate(String date){
+		SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+		try {
+			return format.parse(date);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+	
 	private Article extractArticle(String url){
 		try {
 			Document document = Jsoup.connect(url).userAgent(chromeUserAgent).get();
 			String title = document.title().substring(10);
 			String content = document.body().select("div[class=single-entry-text bbtext]").text();
+			String date = document.body().select("span.time").text();
+			
 			List<IComment> comments = new LinkedList<IComment>();
 			
-			LinkedList<String> commentsContent = new LinkedList<String>();
+			LinkedList<String> commentContents = new LinkedList<String>();
+			LinkedList<String> commentDates = new LinkedList<String>();
 			
-			for(Element e: document.body().select("div[class=comments-popular]").select("div[class=comment-text]")){
-				commentsContent.add(e.text());	
+			Elements tmp = document.body().select("div[class=comments-popular]");
+			
+			for(Element e: tmp.select("div[class=comment-text]")){
+				commentContents.add(e.text());	
+			}
+			
+			for(Element e: tmp.select("span.comment-date")){
+				commentDates.add(e.text().split(" ")[0]);	
 			}
 			
 			int index=0;
 			
-			for(Element e: document.body().select("div[class=comments-popular]").select("span.comment-author")){
+			for(Element e: tmp.select("span.comment-author")){
 				String commentAuthor = e.text();
-				comments.add(new Comment(commentsContent.get(index++),commentAuthor,new Date()));
+				comments.add(new Comment(commentContents.get(index),commentAuthor, parseDate(commentDates.get(index++))));
 			}
 			
-			return new Article(title, content, new Date(), comments);
+			Date formatedDate = null;
+			if(date.split(" ").length>1){
+				formatedDate = parseDate(date.split(" ")[1]);
+			}
+			
+			return new Article(title, content, formatedDate, comments);
 		} catch (IOException e) {
 			return null;
 		}
