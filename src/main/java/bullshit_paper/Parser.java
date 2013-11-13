d9f2b9b339a93672f4b7202ec5431d28d2811b3d
Marcin <dziadus.marcin@gmail.com>
2013-11-14 00:52:30 +0100

    Add a simple parser

---
+++ src/main/java/bullshit_paper/Parser.java
@@ -0,0 +1,50 @@
+package bullshit_paper;
+
+import java.text.BreakIterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+public class Parser {
+	static BreakIterator sentenceIterator = BreakIterator.getSentenceInstance(new Locale("pl","PL"));
+	static BreakIterator wordIterator = BreakIterator.getWordInstance(new Locale("pl","PL"));
+	
+	public static List<String> parseText(String text){
+		List<String> sentences = new LinkedList<String>();
+	    sentenceIterator.setText(text);
+	    
+	    int start = sentenceIterator.first();
+	    int end = sentenceIterator.next();
+
+	    while (end != BreakIterator.DONE) {
+	        String sentence = text.substring(start, end);
+	        if (Character.isLetterOrDigit(sentence.charAt(0))) {
+	            sentences.add(sentence);
+	        }
+	        
+	        start = end;
+	        end = sentenceIterator.next();
+	    }
+		return sentences;
+	}
+	
+	public static List<String> parseSentence(String sentence){
+		List<String> words = new LinkedList<String>();
+	    wordIterator.setText(sentence);
+	    int start = wordIterator.first();
+	    int end = wordIterator.next();
+
+	    while (end != BreakIterator.DONE) {
+	        String word = sentence.substring(start,end);
+	        if (Character.isLetterOrDigit(word.charAt(0))) {
+	        	words.add(word);
+	        }
+	        
+	        start = end;
+	        end = wordIterator.next();
+	    }
+	    
+		return words;
+	}
+	
+}
