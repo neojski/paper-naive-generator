8108d55420e7f9dc5158a36c8654575d2ef0447f
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-11-14 15:42:32 +0100

    Implement basic ArticleMixer using Parser class
    So that word split works nice

--- src/main/java/bullshit_paper/ArticleMixer.java
+++ src/main/java/bullshit_paper/ArticleMixer.java
@@ -1,39 +1,20 @@
 package bullshit_paper;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class ArticleMixer {
-    private class GraphElement {
-        String word;
-    }
-    
+
     public List<IArticle> Mix(List<IArticle> articles) {
-        /*
-        for(IArticle article : articles) {
-            String content = article.getContent();
-            String[] words = content.split("[\\W]");
-            Set<GraphElement> graph = new TreeSet<>();
-            for(String word : words) {
-                
-                if( !graph.contains(word) ) {
-                    graph.add(new GraphElement());
-                }
-            }
-        }
-         * 
-         */
         List<IArticle> newArticles = new LinkedList<>();
-        for(IArticle article : articles) {
+        for (IArticle article : articles) {
             String content = article.getContent();
-            String[] sentences = content.split("[.]");
-            java.util.Collections.shuffle(Arrays.asList(sentences));
+            List<String> sentences = Parser.parseText(content);
+            Collections.shuffle(sentences);
             StringBuilder newContent = new StringBuilder();
-            for(String s : sentences) {
-                newContent.append(s).append(".");
+            for (String s : sentences) {
+                newContent.append(s);
             }
             newArticles.add(new Article(article.getTitle(), newContent.toString(), article.getDate(), article.getComments(), article.getImages()));
         }
