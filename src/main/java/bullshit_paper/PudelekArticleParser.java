2fe86d11384e91e4a668f4f176d75d239f721c77
Marcin <dziadus.marcin@gmail.com>
2013-11-17 13:58:15 +0100

    Replace PudelekArticleSource with *Parser and *Provider

---
+++ src/main/java/bullshit_paper/PudelekArticleParser.java
@@ -0,0 +1,92 @@
+package bullshit_paper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class PudelekArticleParser {
+	
+	public Article parseDocument(Document document){
+		if (document == null) {
+			return null;
+		}
+        
+		String title = document.title();
+		if(title.length()>=10){
+			title = title.substring(10);
+		}
+		else
+			title = null;
+
+		String content = document.body().select("div[class=single-entry-text bbtext]").text();
+		String date = document.body().select("span.time").text();
+
+		Date formatedDate = null;
+		if(date.split(" ").length>1){
+			formatedDate = parseDate(date.split(" ")[1]);
+		}
+		
+		if (content.isEmpty() || title.isEmpty() || date.isEmpty()) {
+            		return null;
+        	}
+		
+		return new Article(title, content, formatedDate, extractComments(document), extractImages(document));
+	}
+	
+	private Date parseDate(String date){
+		SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+		try {
+			return format.parse(date);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+	
+	private List<IImage> extractImages(Document document){
+		List<IImage> images = new LinkedList<IImage>();
+		
+		for(Element e: document.body().select("div[class=single-entry-text bbtext] span.photo img")){
+			try {
+				Image image = new Image(new URL(e.attr("src")));
+				images.add(image);
+			} catch (IOException ex) {
+			}
+		}
+		
+		return images;
+	}
+
+	private List<IComment> extractComments(Document document){
+		List<IComment> comments = new LinkedList<IComment>();
+		
+		LinkedList<String> commentContents = new LinkedList<String>();
+		LinkedList<String> commentDates = new LinkedList<String>();
+		
+		Elements tmp = document.body().select("div[class=comments-popular]");
+		
+		for(Element e: tmp.select("div[class=comment-text]")){
+			commentContents.add(e.text());	
+		}
+		
+		for(Element e: tmp.select("span.comment-date")){
+			commentDates.add(e.text().split(" ")[0]);	
+		}
+		
+		int index=0;
+		
+		for(Element e: tmp.select("span.comment-author")){
+			String commentAuthor = e.text();
+			comments.add(new Comment(commentContents.get(index),commentAuthor, parseDate(commentDates.get(index++))));
+		}	
+		
+		return comments;
+	}
+}
---
+++ src/main/java/bullshit_paper/PudelekArticleProvider.java
@@ -0,0 +1,57 @@
+package bullshit_paper;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class PudelekArticleProvider {
+	private final String chromeUserAgent = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
+	private final String searchForm = "http://www.pudelek.pl/szukaj";
+	
+	public List<Document> getDocuments(List<String> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return null;
+        }
+        
+		LinkedList<Document> documents = new LinkedList<Document>();
+		StringBuilder query = new StringBuilder();
+		
+		for(String tag: tags) query.append(tag).append("+");
+		
+		if(query.length()>0){
+			query.deleteCharAt(query.length()-1);
+		}
+		
+		Document document;
+		try {
+			document = Jsoup.connect(searchForm + query.toString()).userAgent(chromeUserAgent).get();
+			HashSet<String> links = new HashSet<String>();
+
+			for (Element element : document.body().select("div[class=articles clearfix] a")){
+				if(element.attr("href").contains("/tag/")){
+					continue;
+				}
+				if(element.attr("href").contains("/#add_comment")){
+					continue;
+				}
+				links.add(element.attr("href"));
+			}
+			
+			for(String link: links){
+				if(link != null){
+					Document article = Jsoup.connect(link).userAgent(chromeUserAgent).get();
+					documents.add(article);
+				}
+			}
+		} catch (IOException e1) {
+			return null;
+		}
+		
+		return documents;
+	}
+}
